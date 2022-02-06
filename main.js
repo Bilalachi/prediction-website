81b2f3494d7012c0f1eb534fa1fa2ec7561435b7
Bilal Achi <Bilal.achi94@gmail.comgit remote add origin https://github.com/Bilalachi/dog_dating_websitegit push -u origin maingit config --global user.email Bilal.achi94@gmail.com>
2022-02-06 03:10:06 +0200

    nationality prediction

--- main.js
+++ main.js
@@ -13,11 +13,19 @@ let input = document.querySelector("input")
 let button = document.getElementById("prediction-button");
 
 button.addEventListener("click", function () {
-    predict_Gender(input.value)
+    predict_Gender(input.value), predict_Nationality()
   });
 
   async function predict_Gender() {
     let gender_result = await fetch ("https://api.genderize.io/?name=" + input.value);
     let str1 = await gender_result.json()
     result1.innerHTML = str1.gender
+}
+
+async function predict_Nationality() {
+    let nationality_result = await fetch ("https://api.nationalize.io/?name="+ input.value);
+    let str2 = await nationality_result.json()  
+    let list = [];
+    list = `${str2.country[0].country_id}, ${str2.country[1].country_id}, ${str2.country[2].country_id}`
+    result2.innerHTML = list
 }
