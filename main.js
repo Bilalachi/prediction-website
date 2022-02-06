4f0a86bdc7b564721cf40c57293b98c96049d726
Bilal Achi <Bilal.achi94@gmail.comgit remote add origin https://github.com/Bilalachi/dog_dating_websitegit push -u origin maingit config --global user.email Bilal.achi94@gmail.com>
2022-02-06 03:02:46 +0200

    random dog api done

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("it's working")
+
+async function dogphoto() {
+    let photoresult = await fetch("https://dog.ceo/api/breeds/image/random");
+    let photo = await photoresult.json();
+    document.getElementById("imageRandom").src = `${photo.message}`;
+
+}
+
+dogphoto()
