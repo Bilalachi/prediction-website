40b6056922f6457b63371e64da884b65f052c405
Bilal Achi <Bilal.achi94@gmail.comgit remote add origin https://github.com/Bilalachi/dog_dating_websitegit push -u origin maingit config --global user.email Bilal.achi94@gmail.com>
2022-02-07 06:05:22 +0200

    fixed responsiveness

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 console.log("it's working")
 
+// getting random dog pic
 async function dogphoto() {
     let photoresult = await fetch("https://dog.ceo/api/breeds/image/random");
     let photo = await photoresult.json();
@@ -12,16 +13,21 @@ dogphoto()
 let input = document.querySelector("input")
 let button = document.getElementById("prediction-button");
 
+//starting predictions and adding styles to results
 button.addEventListener("click", function () {
+    document.getElementById("predictions").style.borderColor = "black";
+    document.getElementById("predictions").style.boxShadow = "3px 3px 3px 3px grey";
     predict_Gender(), predict_Nationality() ,predict_age()
   });
 
+  //get gender
   async function predict_Gender() {
     let gender_result = await fetch ("https://api.genderize.io/?name=" + input.value);
     let str1 = await gender_result.json()
     result1.innerHTML = str1.gender
 }
 
+//get age
 async function predict_age() {
     let age_result = await fetch ("https://api.agify.io/?name=" + input.value);
     let str3 = await age_result.json()
@@ -29,6 +35,7 @@ async function predict_age() {
     
 }
 
+//get Nationality
 async function predict_Nationality() {
     let nationality_result = await fetch ("https://api.nationalize.io/?name="+ input.value);
     let str2 = await nationality_result.json()  
@@ -36,3 +43,12 @@ async function predict_Nationality() {
     list = `${str2.country[0].country_id}, ${str2.country[1].country_id}, ${str2.country[2].country_id}`
     result2.innerHTML = list
 }
+
+//remove the results
+
+document.getElementById("name").addEventListener("mouseenter", function() {
+    document.getElementById("result1").innerHTML = ""
+    document.getElementById("result2").innerHTML = ""
+    document.getElementById("result3").innerHTML = ""
+    document.getElementById("predictions").style.boxShadow ="0px 0px 0px 0px" 
+})
